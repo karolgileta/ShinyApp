847acde8803c1f297e424b06d17b88557c7f83a7
karolgileta <karol.gileta@gmail.com>
2017-10-26 19:32:14 +0200

    final corrections of logo

--- R/addCompanyLogo.R
+++ R/addCompanyLogo.R
@@ -1,4 +1,4 @@
-add7NWatermark = function(file.name,
+addCompanyLogo = function(file.name,
                           file.width,
                           file.height,
                           watermark.path = "inst/7nlogo.png",
@@ -6,11 +6,11 @@ add7NWatermark = function(file.name,
                           y.coordinate = 0.17,
                           width = 0.2,
                           plot.unit = 'npc') {
-  #' @title Add 7N watermark to graph
-  #' @name add7NWatermark
+  #' @title Add company logo to graph
+  #' @name addCompanyLogo
   #'
   #' @description
-  #' \code{add7NWatermark} adds 7N watermark to given graph
+  #' \code{addCompanyLogo} adds company logo to given graph
   #'
   #' @param file.name name of the file
   #' @param file.width name of the graph PNG file
@@ -21,15 +21,15 @@ add7NWatermark = function(file.name,
   #' @param width width of the watermark
   #' @param plot.unit unit used in function
   #'
-  #' @usage add7NWatermark(file.name, file.width, file.height,
+  #' @usage addCompanyLogo(file.name, file.width, file.height,
   #'  watermark.path, x.coordinate, y.coordinate, width, plot.unit)
   #'
   #' @return png with added watermark
   #' @examples
   #' test = system.file("extdata", "test.png", package = "ShinyApp")
   #' logo = system.file("extdata", "logo.png", package = "ShinyApp")
-  #' add7NWatermark(test, 1200, 800, logo, 0.84, 0.17, 0.02, 'npc')
-  #' @export add7NWatermark
+  #' addCompanyLogo(test, 1200, 800, logo, 0.84, 0.17, 0.02, 'npc')
+  #' @export addCompanyLogo
   #' @import png
   #' @import grDevices
   #' @import grid
--- tests/testthat/test_addCompanyLogo.R
+++ tests/testthat/test_addCompanyLogo.R
@@ -2,6 +2,6 @@ library(testthat)
 library(ShinyApp)
 
 test_that("Correct file.name argument in function", {
-  expect_error(add7NWatermark(),
+  expect_error(addCompanyLogo(),
                'argument "file.name" is missing, with no default')
 })
